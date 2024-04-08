d81faa26dd68b814e409c0b50779942174a0d7e9
RodrigoKraxis <163210320+RodrigoKraxis@users.noreply.github.com>
2024-04-08 16:04:26 -0400

    Js (#5)
    * se completan cambios principales en css
    
    * se completan cambios principales en css parte 2
    
    * se agrega script y se finalizan cambios de css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    /*Tooltip en imagenes de tarjetas*/
+    $("#tarjeta1, #tarjeta2, #tarjeta3, #tarjeta4").hover(function () {
+        $(this).tooltip();
+        $(this).tooltip("open");
+    });
+    /*Se agrega modal a boton de formulario*/
+    const myModal = document.getElementById('myModal')
+    const myInput = document.getElementById('myInput')
 
-
-
-
-
-
-
-
+    myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus()
+    })
 });
